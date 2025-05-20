74d48760d621f3d87392c822ef542107bb719406
rajithlingam <rajithlingamedu@gmail.com>
2025-05-20 12:26:18 +0530

    Second commit

---
+++ JS_DSA/Array/index.js
@@ -0,0 +1,7 @@
+// Creating an Array and Initializing with Values
+let a = ["HTML", "CSS", "JS"];
+
+// Accessing Last Array Elements
+let lst = a[a.length - 1];
+
+console.log("First Item: ", lst);
